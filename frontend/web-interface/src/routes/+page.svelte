df71671e77e1578a88f16ff0d8e199766acb78ec
Magura-Witkowski,Stanisław S.M. <s.magurawitkowski@student.fontys.nl>
2025-10-01 11:40:45 +0200

    temporary navigation, added to test the new front-end pages

--- frontend/web-interface/src/routes/+page.svelte
+++ frontend/web-interface/src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script>
-  import PatientDashboard from '$lib/pages/App.svelte';
+  import PatientView from '$lib/pages/patientView.svelte';
+  import AdminView from '$lib/pages/adminView.svelte';
+  import DoctorView from '$lib/pages/doctorView.svelte';
+  import LogIn from '$lib/pages/logIn.svelte';
+
+  let view = ''; // '', 'patient', 'admin', 'doctor', 'login'
+  const go = (v) => { view = v; };
 </script>
 
-<PatientDashboard />
+<a href="#" on:click|preventDefault={() => go('patient')}>patientView</a><br>
+<a href="#" on:click|preventDefault={() => go('admin')}>adminView</a><br>
+<a href="#" on:click|preventDefault={() => go('login')}>logIn</a><br>
+<a href="#" on:click|preventDefault={() => go('doctor')}>doctorView</a>
+
+<hr>
+
+{#if view === 'patient'}
+  <PatientView />
+{:else if view === 'admin'}
+  <AdminView />
+{:else if view === 'login'}
+  <LogIn />
+{:else if view === 'doctor'}
+  <DoctorView />
+{:else}
+  <p>Choose a page above.</p>
+{/if}
